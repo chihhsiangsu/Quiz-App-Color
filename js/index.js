755dee23510a77a27721fe868593c3e8e7f73b8e
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-03-13 19:08:12 +0100

    dark mode creating

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const darkModeButton = document.querySelector('[data-js="colorSwitch"]');
+console.log(darkModeButton);
+const body = document.querySelector('[data-js="body"]');
+const cards = document.querySelectorAll('[data-js="card"]');
+
+darkModeButton.addEventListener("click", () => {
+  body.classList.toggle("body--dark");
+
+  cards.forEach((card) => {
+    card.classList.toggle("card--dark");
+  });
+});
