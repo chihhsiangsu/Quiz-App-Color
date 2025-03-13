31585c2ac3be70398f69b2afbc44c3610154d042
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-03-13 21:06:33 +0100

    add theme js page

--- js/index.js
+++ js/index.js
@@ -1,17 +1,1 @@
-const darkModeButton = document.querySelector('[data-js="colorSwitch"]');
-console.log(darkModeButton);
-const body = document.querySelector('[data-js="body"]');
-const cards = document.querySelectorAll('[data-js="card"]');
 
-darkModeButton.addEventListener("click", () => {
-  body.classList.toggle("body--dark");
-  darkModeButton.classList.toggle("header__colorSwitch--dark");
-
-  cards.forEach((card) => {
-    card.classList.toggle("card--dark");
-
-    darkModeButton.textContent = body.classList.contains("body--dark")
-      ? "Light Mode"
-      : "Dark Mode";
-  });
-});
